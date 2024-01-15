2c9b6f32e959e5cdd73e61bbcb789269675ad913
kullph <kullaphat01.tr@gmail.com>
2024-01-15 15:48:29 +0700

    init

---
+++ Cn334_W02_InClass/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Cn334_W02_InClass project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cn334_W02_InClass.settings')
+
+application = get_asgi_application()
